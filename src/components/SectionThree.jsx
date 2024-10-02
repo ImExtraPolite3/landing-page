17ed5c15d0dac2416dc90277ed86434fc6f60505
ExtraPolite <imextrapolite2@gmail.com>
2024-10-02 15:44:30 -0400

    add ids for quotes and author

--- src/components/SectionThree.jsx
+++ src/components/SectionThree.jsx
@@ -1,12 +1,12 @@
 export default function SectionThree() {
   return (
     <section id="section-three" className="size">
-      <p>
+      <p id="quote">
         This is an inspiring quote, or a testimonial from a customer. Maybe it’s
         just filling up space, or maybe people will actually read it. Who knows?
         All I know is that it looks nice.
       </p>
-      <p>- Thor, God of Thunder</p>
+      <p id="author">- Thor, God of Thunder</p>
     </section>
   );
 }
