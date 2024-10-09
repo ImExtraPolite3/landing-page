4d6872bcc0b6367e46e4720f85f8775b2464493d
ExtraPolite <imextrapolite2@gmail.com>
2024-10-09 15:38:28 -0400

    show nav buttons when menu button is clicked

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,6 +6,16 @@ function NavBarButtons({ name }) {
   );
 }
 
+function showHeaderButtons() {
+  const navButtons = document.getElementById('nav-bar-right');
+
+  if (navButtons.checkVisibility() == false) {
+    navButtons.style.display = 'block';
+  } else {
+    navButtons.style.display = 'none';
+  }
+}
+
 function NavBar() {
   return (
     <header id="nav-bar" className="size">
@@ -13,7 +23,7 @@ function NavBar() {
         <h1>Header Logo</h1>
       </div>
       <div id="menu">
-        <img src="menu.svg" alt="" />
+        <img src="menu.svg" alt="" onClick={showHeaderButtons} />
       </div>
       <div id="nav-bar-right">
         <NavBarButtons key={1} name="one" />
