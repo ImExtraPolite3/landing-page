d05e39f98e54b1e43dc02c41fa6f888ed16e14c1
ExtraPolite <imextrapolite2@gmail.com>
2024-10-10 16:15:09 -0400

    add class for hiding header links. Edit function to correctly hide and show header button when menu button is shown

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,13 +6,15 @@ function NavBarButtons({ name }) {
   );
 }
 
-function showHeaderButtons() {
+function showHeaderButtonsNarrow() {
   const navButtons = document.getElementById('nav-bar-right');
 
   if (navButtons.checkVisibility() == false) {
-    navButtons.style.display = 'block';
+    navButtons.classList.add('show-nav-buttons');
+    navButtons.classList.remove('hide-nav-buttons');
   } else {
-    navButtons.style.display = 'none';
+    navButtons.classList.remove('show-nav-buttons');
+    navButtons.classList.add('hide-nav-buttons');
   }
 }
 
@@ -23,9 +25,9 @@ function NavBar() {
         <h1>Header Logo</h1>
       </div>
       <div id="menu">
-        <img src="menu.svg" alt="" onClick={showHeaderButtons} />
+        <img src="menu.svg" alt="" onClick={showHeaderButtonsNarrow} />
       </div>
-      <div id="nav-bar-right">
+      <div id="nav-bar-right" className="hide-nav-buttons">
         <NavBarButtons key={1} name="one" />
         <NavBarButtons key={2} name="two" />
         <NavBarButtons key={3} name="three" />
